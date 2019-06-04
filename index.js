0270e74ac0cb193b45c617aa40f78bd49e3e0e88
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-04 17:39:00 -0400

    removed token

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
 const config = require("./botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
+const token = process.env.token;
 module.exports = bot;
 
 const {loadCommands, loadEvents} = require("./util/handler");
 loadCommands();
 loadEvents();
 
-bot.login(config.token);
+bot.login(token);

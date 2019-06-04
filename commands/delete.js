3fde3e0fd8efbf9f28ec4b566baf7ec4cb820986
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-04 18:35:21 -0400

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -11,9 +11,8 @@ module.exports.command = {
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Invalid Permissions");
     if (!args[0]) return message.channel.send("Specify delete amount");
-    let num = args[0] + 1;
+    let num = parseInt(args[0]) + 1;
     message.channel.bulkDelete(num).then(() => {
-        message.channel.bulkDelete(1);
         message.channel.send(`Deleted ${args[0]} messages`).then(msg => msg.delete(3000));
     });
 }

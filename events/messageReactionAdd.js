f683090f0333de3149ad67d528fbe0b20a2b922d
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-04 20:02:48 -0400

    Create messageReactionAdd.js

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const bot = require("../index.js");
+const config = require("../botconfig.json");
+
+bot.on("messageReactionAdd", async (messageReaction, user) => {
+
+    if (messageReaction.message.id == 585616911149170693){
+        if(messageReaction.emoji.name == "headphones"){
+            user.addRole(585617992549793822);
+        }
+    }
+});

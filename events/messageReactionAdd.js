5294c875e54c0800d75625aa6fb7ab886c06662d
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-04 20:52:16 -0400

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -6,7 +6,7 @@ bot.on("messageReactionAdd", async (client, messageReaction, user) => {
 
     if (messageReaction.message.id == 585616911149170693){
         if(messageReaction.emoji.name == ":headphones:"){
-            user.addRole(messageReaction.guild.roles.find('name', 'Producer').id);
+            user.addRole(585617992549793822);
         }
     }
 });

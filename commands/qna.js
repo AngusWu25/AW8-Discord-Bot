2284db162705a4a94d7bb2bcf670bbe5e5779011
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-16 00:37:48 -0400

    lkjh

---
+++ commands/qna.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.command = {
+    name: "qna",
+    aliases: ["qna"],
+    description: "delete messages",
+    category: "",
+    usage: "delete #"
+}
+
+module.exports.run = async (bot, message, args) => {
+    const questSub = message.guild.channels.find(c => c.name === 'question-submissions');
+    let botmessage = args.join(" ");
+    let qembed = new Discord.RichEmbed()
+        .setTitle("Question")
+        .setDescription(botmessage)
+        .addField("Message:", `${message.content}`)
+        .addField("Question From", `<@${message.author.id}>`)
+        .setColor("#FFFF00")
+
+    questsub.send(qembed).then(message => {
+        message.react(":white_check_mark:")
+        message.react(":x:")
+    })
+    
+}
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -4,27 +4,27 @@ const config = require("../botconfig.json");
 
 bot.on("messageReactionAdd", (messageReaction, user) => {
     
-    let roleName = messageReaction.emoji.name;
-    let role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase());
+    // let roleName = messageReaction.emoji.name;
+    // let role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase());
 
-    if(role){
-        let member = messageReaction.message.guild.members.find(member => member.id === user.id);
-        if(member){
-            member.addRole(role.id);
-        }
-    }
-
-    // if(!user) return;
-    // if(user.bot) return;
-    // if(!reaction.message.channel.guild) return;
-
-    // let emojiname = ["smiley", "eyes"];
-    // let rolename = ["testing 1", "testing 2"];
-
-    // for(let n in emojiname){
-    //     if(reaction.emoji.name == emojiname[n]){
-    //         let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);
-    //         reaction.message.guild.member(user).addRole(role).catch(console.error);
+    // if(role){
+    //     let member = messageReaction.message.guild.members.find(member => member.id === user.id);
+    //     if(member){
+    //         member.addRole(role.id);
     //     }
     // }
+
+    if(!user) return;
+    if(user.bot) return;
+    if(!reaction.message.channel.guild) return;
+
+    let emojiname = ["smiley", "eyes"];
+    let rolename = ["testing 1", "testing 2"];
+
+    for(let n in emojiname){
+        if(reaction.emoji.name == emojiname[n]){
+            let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);
+            reaction.message.guild.member(user).addRole(role).catch(console.error);
+        }
+    }
 });

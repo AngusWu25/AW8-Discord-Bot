6148dee489eff78433b2717acf096bde9f13cce3
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-15 22:09:22 -0400

    Create messageReactionAdd.js

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = require("../index.js");
+const config = require("../botconfig.json");
+
+bot.on("messageReactoinAdd", async(reaction, user) => {
+    if(!user) return;
+    if(user.bot) return;
+    if(!reaction.message.channel.guild) return;
+
+    let emojiname = [":smiley:", ":eyes:"];
+    let rolename = ["testing 1", "testing 2"];
+
+    for(let n in emojiname){
+        if(reaction.emoji.name == emojiname[n]){
+            let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);
+            reaction.message.guild.member(user).addRole(role).catch(console.error);
+        }
+    }
+});

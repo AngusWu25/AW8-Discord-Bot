907069b9a10877e8dc597f5f669160eeba0c230e
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-15 22:19:01 -0400

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,17 +3,18 @@ const bot = require("../index.js");
 const config = require("../botconfig.json");
 
 bot.on("messageReactoinAdd", (messageReaction, user) => {
-    if(!user) return;
-    if(user.bot) return;
-    if(!reaction.message.channel.guild) return;
+    console.log(user.username + "reacted");
+    // if(!user) return;
+    // if(user.bot) return;
+    // if(!reaction.message.channel.guild) return;
 
-    let emojiname = ["smiley", "eyes"];
-    let rolename = ["testing 1", "testing 2"];
+    // let emojiname = ["smiley", "eyes"];
+    // let rolename = ["testing 1", "testing 2"];
 
-    for(let n in emojiname){
-        if(reaction.emoji.name == emojiname[n]){
-            let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);
-            reaction.message.guild.member(user).addRole(role).catch(console.error);
-        }
-    }
+    // for(let n in emojiname){
+    //     if(reaction.emoji.name == emojiname[n]){
+    //         let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);
+    //         reaction.message.guild.member(user).addRole(role).catch(console.error);
+    //     }
+    // }
 });

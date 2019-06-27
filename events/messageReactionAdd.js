2988679c736649fcc26f1f76c6d87083f24af07c
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-06-27 14:31:05 -0400

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -14,6 +14,7 @@ bot.on("messageReactionAdd", (messageReaction, user) => {
     //     }
     // }
 
+    console.log("emoji");
     if(!user) return;
     if(user.bot) return;
     //if(!messageReaction.message.channel.guild) return;

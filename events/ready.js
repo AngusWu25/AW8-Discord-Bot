f33a8b3d3192cdc01767649455dfced626f8d26f
Walker431 <51384855+Walker431@users.noreply.github.com>
2019-07-19 11:26:00 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const bot = require("../index.js");
 
 bot.on("ready", async () => {
     console.log('bot is online!');
-    bot.user.setActivity("to Unity?", {type: "LISTENING"});
+    bot.user.setActivity("Unity?", {type: "LISTENING"});
 
     // bot.channels.forEach(channel => channel.fetchMessages({ limit: 100 })
     //     .then(messages => console.log(`Received ${messages.size} messages`))

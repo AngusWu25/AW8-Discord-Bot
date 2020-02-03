937598a816b8555c579b6450f9dad42209e740f2
Walker431 <51384855+Walker431@users.noreply.github.com>
2020-02-03 18:02:45 -0500

    Update messages.js

--- events/messages.js
+++ events/messages.js
@@ -7,14 +7,14 @@ bot.on("message", async message => {
     if (message.channel.type === "dm") return;
     let prefix = config.prefix;
 
-    let bannedWords = ['walkercom', 'walker.com', 'walker com', 'w41k3rc0m', 'w41k3r.com', 'w41k3rcom', 'w41k3r'];
-    let foundInText = false;
-    for (var i in bannedWords) {
-        if (!message.member.hasPermission("MANAGE_MESSAGES") && message.content.toLowerCase().includes(bannedWords[i].toLowerCase())) foundInText = true;
-    }
-    if (foundInText = true) {
-        message.delete();
-    }
+    //let bannedWords = ['walkercom', 'walker.com', 'walker com', 'w41k3rc0m', 'w41k3r.com', 'w41k3rcom', 'w41k3r'];
+    //let foundInText = false;
+    //for (var i in bannedWords) {
+    //    if (!message.member.hasPermission("MANAGE_MESSAGES") && message.content.toLowerCase().includes(bannedWords[i].toLowerCase())) foundInText = true;
+    //}
+    //if (foundInText = true) {
+    //    message.delete();
+    //}
 
     if(message.content.charAt(0) != config.prefix) return;
     let args = message.content.slice(prefix.length).trim().split(/ +/);

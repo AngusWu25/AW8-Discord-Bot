79e96e848b15d9e1ba16def0a3af5ddf1c564bed
Walker431 <51384855+Walker431@users.noreply.github.com>
2020-11-01 10:20:32 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const bot = require("../index.js");
 
 bot.on("ready", async () => {
     console.log('bot is online!');
-    bot.user.setActivity("Heading Home", {type: "LISTENING"});
+    bot.user.setActivity("AWT's Mashup", {type: "LISTENING"});
 
     // bot.channels.forEach(channel => channel.fetchMessages({ limit: 100 })
     //     .then(messages => console.log(`Received ${messages.size} messages`))
